c2d78b782b27de3a03909fa98c5819183f25e1ba
Tom Warner <tomwarner13@gmail.com>
2024-12-11 09:03:42 -0500

    day 11

--- AdventOfCode2024/Day11/Day11Problems.cs
+++ AdventOfCode2024/Day11/Day11Problems.cs
@@ -4,292 +4,74 @@ namespace AdventOfCode2024.Day11;
 
 public class Day11Problems : Problems
 {
-  protected override string TestInput => @"Monkey 0:
-  Starting items: 79, 98
-  Operation: new = old * 19
-  Test: divisible by 23
-    If true: throw to monkey 2
-    If false: throw to monkey 3
-
-Monkey 1:
-  Starting items: 54, 65, 75, 74
-  Operation: new = old + 6
-  Test: divisible by 19
-    If true: throw to monkey 2
-    If false: throw to monkey 0
-
-Monkey 2:
-  Starting items: 79, 60, 97
-  Operation: new = old * old
-  Test: divisible by 13
-    If true: throw to monkey 1
-    If false: throw to monkey 3
-
-Monkey 3:
-  Starting items: 74
-  Operation: new = old + 3
-  Test: divisible by 17
-    If true: throw to monkey 0
-    If false: throw to monkey 1";
+  protected override string TestInput => @"125 17";
 
   protected override int Day => 11;
 
   protected override string Problem1(string[] input, bool isTestInput)
   {
-    var monkeyList = new List<Monkey>();
+    var resultsCache = new Dictionary<LongPoint, long>();
 
-    Monkey curMonkey = null;
+    var line = input[0];
+    var startNumbers = StringUtils.ExtractIntsFromString(line);
+    var total = 0L;
+    const int blinks = 25;
 
-    foreach (var rawLine in input)
-    {
-      var line = rawLine.Trim();
-
-      if (line.StartsWith("Monkey "))
-      {
-        curMonkey = new Monkey();
-      }
-      else if (line.StartsWith("Starting items:"))
-      {
-        curMonkey.AddItems(ParseStartingItems(line));
-      }
-      else if (line.StartsWith("Operation: new = "))
-      {
-        curMonkey.SetInspection(ParseOperation(line));
-      }
-      else if (line.StartsWith("Test: divisible by "))
-      {
-        var raw = line.Replace("Test: divisible by ", "");
-        curMonkey.SetTestDivisor(int.Parse(raw));
-      }
-      else if (line.StartsWith("If true: throw to monkey "))
-      {
-        var raw = line.Replace("If true: throw to monkey ", "");
-        curMonkey.SetTrueTarget(int.Parse(raw));
-      }
-      else if (line.StartsWith("If false: throw to monkey "))
-      {
-        var raw = line.Replace("If false: throw to monkey ", "");
-        curMonkey.SetFalseTarget(int.Parse(raw));
-      }
-      else if (string.IsNullOrWhiteSpace(line))
-      {
-        monkeyList.Add(curMonkey);
-      }
-      else
-      {
-        throw new ArgumentException("invalid: " + line);
-      }
+    foreach (var startNumber in startNumbers)
+    { 
+      total += BlinkForNumber(startNumber, blinks, ref resultsCache);
     }
 
-    //there's a last monkey without corresponding whitespace
-    monkeyList.Add(curMonkey);
-
-    var monkeys = monkeyList.ToArray();
-
-    const int totalRounds = 20;
-
-    for (var i = 0; i < totalRounds; i++)
-    {
-      foreach (var monkey in monkeys)
-      {
-        Perform1Round(monkey, monkeys);
-      }
-    }
-
-    var inspectCounts = monkeys.Select(m => m.GetActivity());
-
-    var orderedCounts = inspectCounts.OrderByDescending(i => i).ToArray();
-
-    return (orderedCounts[0] * orderedCounts[1]).ToString();
+    return total.ToString();
   }
 
   protected override string Problem2(string[] input, bool isTestInput)
   {
-    var monkeyList = new List<Monkey>();
+    var resultsCache = new Dictionary<LongPoint, long>();
 
-    Monkey curMonkey = null;
-    var commonMultiple = 1;
+    var line = input[0];
+    var startNumbers = StringUtils.ExtractIntsFromString(line);
+    var total = 0L;
+    const int blinks = 75;
 
-    foreach (var rawLine in input)
-    {
-      var line = rawLine.Trim();
-
-      if (line.StartsWith("Monkey "))
-      {
-        curMonkey = new Monkey();
-      }
-      else if (line.StartsWith("Starting items:"))
-      {
-        curMonkey.AddItems(ParseStartingItems(line));
-      }
-      else if (line.StartsWith("Operation: new = "))
-      {
-        curMonkey.SetInspection(ParseOperation(line));
-      }
-      else if (line.StartsWith("Test: divisible by "))
-      {
-        var raw = line.Replace("Test: divisible by ", "");
-        var divisor = int.Parse(raw);
-        commonMultiple *= divisor;
-
-        curMonkey.SetTestDivisor(divisor);
-      }
-      else if (line.StartsWith("If true: throw to monkey "))
-      {
-        var raw = line.Replace("If true: throw to monkey ", "");
-        curMonkey.SetTrueTarget(int.Parse(raw));
-      }
-      else if (line.StartsWith("If false: throw to monkey "))
-      {
-        var raw = line.Replace("If false: throw to monkey ", "");
-        curMonkey.SetFalseTarget(int.Parse(raw));
-      }
-      else if (string.IsNullOrWhiteSpace(line))
-      {
-        monkeyList.Add(curMonkey);
-      }
-      else
-      {
-        throw new ArgumentException("invalid: " + line);
-      }
+    foreach (var startNumber in startNumbers)
+    { 
+      total += BlinkForNumber(startNumber, blinks, ref resultsCache);
     }
 
-    //there's a last monkey without corresponding whitespace
-    monkeyList.Add(curMonkey);
-
-    var monkeys = monkeyList.ToArray();
-
-    const int totalRounds = 10000;
-
-    for (var i = 0; i < totalRounds; i++)
-    {
-      foreach (var monkey in monkeys)
-      {
-        Perform1Round(monkey, monkeys, commonMultiple);
-      }
-    }
-
-    var inspectCounts = monkeys.Select(m => m.GetActivity());
-
-    var orderedCounts = inspectCounts.OrderByDescending(i => i).ToArray();
-
-    return ((long)orderedCounts[0] * (long)orderedCounts[1]).ToString();
+    return total.ToString();
   }
 
-  private static void Perform1Round(Monkey curMonkey, Monkey[] allMonkeys, int commonMultiple = 0)
+  private static long BlinkForNumber(long curNumber, int blinksRemaining, ref Dictionary<LongPoint, long> resultsCache)
   {
-    while (curMonkey.CanInspect())
+    if (blinksRemaining == 0) return 1;
+    
+    var curIndex = new LongPoint(curNumber, blinksRemaining);
+    if(resultsCache.TryGetValue(curIndex, out var answer)) return answer;
+
+    if (curNumber == 0)
     {
-      var result = curMonkey.InspectItem(commonMultiple);
-      allMonkeys[result.targetMonkey].AddItem(result.returnedItemLevel);
+      var zeroResult = BlinkForNumber(1, blinksRemaining - 1, ref resultsCache);
+      resultsCache[curIndex] = zeroResult;
+      return zeroResult;
     }
-  }
-
-  private static IEnumerable<int> ParseStartingItems(string input)
-  {
-    var raw = input.Replace("Starting items: ", "");
-    var items = raw.Split(", ");
-    return items.Select(int.Parse);
-  }
-
-  private static Func<long, long> ParseOperation(string input)
-  {
-    var raw = input.Replace("Operation: new = ", "");
-    var parts = raw.Split(' ');
-    Func<long, long> lastOperand;
-    if (parts[2] == "old")
+    
+    var strNum = curNumber.ToString();
+    if (strNum.Length % 2 == 0) //split by digits
     {
-      lastOperand = i => i;
+      var firstHalf = strNum[..(strNum.Length / 2)];
+      var secondHalf = strNum[(strNum.Length / 2)..];
+      var firstNum = int.Parse(firstHalf);
+      var secondNum = int.Parse(secondHalf);
+      
+      var splitResult = BlinkForNumber(firstNum, blinksRemaining - 1, ref resultsCache) + 
+                        BlinkForNumber(secondNum, blinksRemaining - 1, ref resultsCache);
+      resultsCache[curIndex] = splitResult;
+      return splitResult;
     }
-    else
-    {
-      lastOperand = i => int.Parse(parts[2]);
-    }
-
-    return parts[1] switch
-    {
-      "+" => i => i + lastOperand(i),
-      "-" => i => i - lastOperand(i),
-      "*" => i => i * lastOperand(i),
-      "/" => i => i / lastOperand(i)
-    };
-  }
-
-  private class Monkey
-  {
-    private readonly Queue<long> _items;
-    private Func<long, long> _inspection;
-    private int _testDivisor;
-    private int _trueTarget;
-    private int _falseTarget;
-    private int _inspectCount;
-
-    public Monkey()
-    {
-      _items = new Queue<long>();
-      _inspection = i => i;
-      _testDivisor = 0;
-      _trueTarget = 0;
-      _falseTarget = 0;
-      _inspectCount = 0;
-    }
-
-    public bool CanInspect() => _items.TryPeek(out _);
-
-    public (int targetMonkey, long returnedItemLevel) InspectItem(int commonMultiple = 0)
-    {
-      _inspectCount++;
-      var item = _items.Dequeue();
-      item = _inspection(item); //inspect
-      if (commonMultiple == 0)
-      {
-        item /= 3; //get bored
-      }
-      else
-      {
-        item %= commonMultiple; //manage size using fancy nerd math
-      }
-
-      if (item % _testDivisor == 0)
-      {
-        return (_trueTarget, item);
-      }
-      return (_falseTarget, item);
-    }
-
-    public void AddItem(long item)
-    {
-      _items.Enqueue(item);
-    }
-
-    public void AddItems(IEnumerable<int> items)
-    {
-      foreach (var item in items)
-      {
-        _items.Enqueue(item);
-      }
-    }
-
-    public void SetInspection(Func<long, long> ins)
-    {
-      _inspection = ins;
-    }
-
-    public void SetTestDivisor(int t)
-    {
-      _testDivisor = t;
-    }
-
-    public void SetTrueTarget(int t)
-    {
-      _trueTarget = t;
-    }
-
-    public void SetFalseTarget(int f)
-    {
-      _falseTarget = f;
-    }
-
-    public int GetActivity() => _inspectCount;
+    
+    var defaultResult = BlinkForNumber(curNumber * 2024, blinksRemaining - 1, ref resultsCache);
+    resultsCache[curIndex] = defaultResult;
+    return defaultResult;
   }
 }
--- AdventOfCode2024/Program.cs
+++ AdventOfCode2024/Program.cs
@@ -1,9 +1,9 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
-using AdventOfCode2024.Day10;
+using AdventOfCode2024.Day11;
 using AdventOfCode2024.Util;
 
-var problems = new Day10Problems();
+var problems = new Day11Problems();
 DoAllProblems(problems);
 return;
 
