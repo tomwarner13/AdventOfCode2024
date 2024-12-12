6b766c90733b4b1f1cf06b100a5d53beaba034fe
Tom Warner <tomwarner13@gmail.com>
2024-12-12 18:24:26 -0500

    day 12

--- AdventOfCode2024/Day12/Day12Problems.cs
+++ AdventOfCode2024/Day12/Day12Problems.cs
@@ -1,4 +1,5 @@
-﻿using AdventOfCode2024.Util;
+﻿using System.Net;
+using AdventOfCode2024.Util;
 
 namespace AdventOfCode2024.Day12;
 
@@ -40,7 +41,28 @@ MMMISSJEEE";
 
   protected override string Problem2(string[] input, bool isTestInput)
   {
-    throw new NotImplementedException();
+    var visitedPoints = new HashSet<GridPoint>();
+    var totalPrice = 0L;
+
+    for (var y = 0; y < input.Length; y++)
+    {
+      for (var x = 0; x < input[y].Length; x++)
+      {
+        var point = new GridPoint(x, y);
+        if (!visitedPoints.Contains(point))
+        {
+          var seed = input[y][x];
+          var pointsInRegion  = GetUniquePointsInRegion(seed, point, ref input, ref visitedPoints);
+          
+          var orderedPoints = pointsInRegion.OrderBy(p => p.Y).ThenBy(p => p.X).ToList();
+
+          var sides = CountSidesOfRegion(orderedPoints);
+          totalPrice += sides * pointsInRegion.Count;
+        }
+      }
+    }
+    
+    return totalPrice.ToString();
   }
 
   private static (int perimeter, int area) CalculateRegion(char seed, GridPoint point, ref string[] input,
@@ -61,5 +83,59 @@ MMMISSJEEE";
     }
     
     return (perimeter, area);
+  }
+
+  private static List<GridPoint> GetUniquePointsInRegion(char seed, GridPoint point, ref string[] input,
+    ref HashSet<GridPoint> visitedPoints)
+  {
+    if (!point.IsInBounds(input[0].Length, input.Length) || input[point.Y][point.X] != seed)
+      return new List<GridPoint>();
+
+    if (!visitedPoints.Add(point)) return new List<GridPoint>();
+
+    var result = new List<GridPoint>{ point };
+    foreach (var dir in GridPoint.CardinalDirections)
+    {
+      result.AddRange(GetUniquePointsInRegion(seed, point + dir, ref input, ref visitedPoints));
+    }
+
+    return result;
+  }
+
+  private static int CountSidesOfRegion(List<GridPoint> orderedRegion)
+  {
+    var sidesByPoint = orderedRegion.ToDictionary(p => p, p => new HashSet<GridPoint>());
+    var uniqueSides = 0;
+
+    foreach (var point in orderedRegion)
+    {
+      foreach(var verticalDir in new List<GridPoint>{GridPoint.Up, GridPoint.Down})
+      {
+        var neighbor = point + verticalDir;
+        if (!sidesByPoint.ContainsKey(neighbor))
+        {
+          //potential side, check if the point to the left is a neighbor and has a corresponding side
+          sidesByPoint[point].Add(verticalDir);
+          var leftNeighbor = point + GridPoint.Left;
+          if (!sidesByPoint.ContainsKey(leftNeighbor) || !sidesByPoint[leftNeighbor].Contains(verticalDir))
+            uniqueSides++;
+        }
+      }
+      
+      foreach(var horizontalDir in new List<GridPoint>{GridPoint.Left, GridPoint.Right})
+      {
+        var neighbor = point + horizontalDir;
+        if (!sidesByPoint.ContainsKey(neighbor))
+        {
+          //potential side, check if the point above is a neighbor and has a corresponding side
+          sidesByPoint[point].Add(horizontalDir);
+          var upNeighbor = point + GridPoint.Up;
+          if (!sidesByPoint.ContainsKey(upNeighbor) || !sidesByPoint[upNeighbor].Contains(horizontalDir))
+            uniqueSides++;
+        }
+      }
+    }
+    
+    return uniqueSides;
   }
 }
