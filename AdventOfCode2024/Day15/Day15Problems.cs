d7fc43404a908e63f54030bd8a811ab8868b2799
Tom Warner <tomwarner13@gmail.com>
2024-12-15 08:38:24 -0500

    day 15 part 1

--- AdventOfCode2024/Day15/Day15Problems.cs
+++ AdventOfCode2024/Day15/Day15Problems.cs
@@ -5,175 +5,120 @@ namespace AdventOfCode2024.Day15;
 
 public class Day15Problems : Problems
 {
-  protected override string TestInput => @"Sensor at x=2, y=18: closest beacon is at x=-2, y=15
-Sensor at x=9, y=16: closest beacon is at x=10, y=16
-Sensor at x=13, y=2: closest beacon is at x=15, y=3
-Sensor at x=12, y=14: closest beacon is at x=10, y=16
-Sensor at x=10, y=20: closest beacon is at x=10, y=16
-Sensor at x=14, y=17: closest beacon is at x=10, y=16
-Sensor at x=8, y=7: closest beacon is at x=2, y=10
-Sensor at x=2, y=0: closest beacon is at x=2, y=10
-Sensor at x=0, y=11: closest beacon is at x=2, y=10
-Sensor at x=20, y=14: closest beacon is at x=25, y=17
-Sensor at x=17, y=20: closest beacon is at x=21, y=22
-Sensor at x=16, y=7: closest beacon is at x=15, y=3
-Sensor at x=14, y=3: closest beacon is at x=15, y=3
-Sensor at x=20, y=1: closest beacon is at x=15, y=3";
+  protected override string TestInput => @"##########
+#..O..O.O#
+#......O.#
+#.OO..O.O#
+#..O@..O.#
+#O#..O...#
+#O..O..O.#
+#.OO.O.OO#
+#....O...#
+##########
+
+<vv>^<v^>v>^vv^v>v<>v^v<v<^vv<<<^><<><>>v<vvv<>^v^>^<<<><<v<<<v^vv^v>^
+vvv<<^>^v^^><<>>><>^<<><^vv^^<>vvv<>><^^v>^>vv<>v<<<<v<^v>^<^^>>>^<v<v
+><>vv>v^v^<>><>>>><^^>vv>v<^^^>>v^v^<^^>v^^>v^<^v>v<>>v^v^<v>v^^<^^vv<
+<<v<^>>^^^^>>>v^<>vvv^><v<<<>^^^vv^<vvv>^>v<^^^^v<>^>vvvv><>>v^<<^^^^^
+^><^><>>><>^^<<^^v>>><^<v>^<vv>>v>>>^v><>^v><<<<v>>v<v<v>vvv>^<><<>^><
+^>><>^v<><^vvv<^^<><v<<<<<><^v<<<><<<^^<v<^^^><^>>^<v^><<<^>>^v<v^v<v^
+>^>>^v>vv>^<<^v<>><<><<v<<v><>v<^vv<<<>^^v^>^^>>><<^v>>v^v><^^>>^<>vv^
+<><^^>^^^<><vvvvv^v<v<<>^v<v>v<<^><<><<><<<^^<<<^<<>><<><^^^>^^<>^>v<>
+^^>vv<^v^v<vv>^<><v<^v>^^^>>>^^vvv^>vvv<>>>^<^>>>>>^<<^v>^vvv<>^<><<v>
+v^^>>><<^^<>>^v^<v^vv<>v^<<>^<^v^v><^<<<><<^<v><v<>vv>>v><v^<vv<>v^<<^";
 
   protected override int Day => 15;
 
   protected override string Problem1(string[] input, bool isTestInput)
   {
-    var validSensors = new List<Sensor>();
-    var beaconsOnHorizontalRow = new HashSet<GridPoint>();
+    var robotPos = new GridPoint(0, 0);
+    var walls = new HashSet<GridPoint>();
+    var boxes = new HashSet<GridPoint>();
+
+    var parsingMap = true;
+    var y = 0;
+
+    while (parsingMap)
+    {
+      var line = input[y];
+      if (!string.IsNullOrWhiteSpace(line))
+      {
+        for (var x = 0; x < line.Length; x++)
+        {
+          switch (line[x])
+          {
+            case '#': walls.Add(new GridPoint(x, y)); break;
+            case 'O': boxes.Add(new GridPoint(x, y)); break;
+            case '@': robotPos = new GridPoint(x, y); break;
+          }
+        }
+      }
+      else
+      {
+        parsingMap = false;
+      }
       
-    var horizontal = isTestInput ? 10 : 2000000;
-
-    foreach (var line in input)
-    {
-      var checkResults = ParseLine(line);
-      if (checkResults.s.IsWithinVerticalRange(horizontal))
-      {
-        validSensors.Add(checkResults.s);
-      }
-
-      if (checkResults.b.Y == horizontal)
-      {
-        beaconsOnHorizontalRow.Add(checkResults.b);
-      }
+      y++;
     }
 
-    var longestDist = validSensors.Select(s => s.Distance).Max();
-    var orderedHorizontally = validSensors.Select(s => s.Location.X).OrderBy(x => x).ToList();
-    var lowestX = orderedHorizontally.First();
-    var highestX = orderedHorizontally.Last();
-
-    var min = lowestX - longestDist;
-    var max = highestX + longestDist;
-
-    var totalExcludedPoints = 0;
-    for (var x = min; x <= max; x++)
+    while (y < input.Length)
     {
-      var pointToCheck = new GridPoint(x, horizontal);
-      if (validSensors.Any(s => s.IsWithinRange(pointToCheck)) & !beaconsOnHorizontalRow.Contains(pointToCheck))
+      var line = input[y];
+      foreach (var c in line)
       {
-        totalExcludedPoints++;
+        var dir = c switch
+        {
+          '^' => GridPoint.Up,
+          'v' => GridPoint.Down,
+          '>' => GridPoint.Right,
+          '<' => GridPoint.Left
+        };
+        AttemptOneMove(ref robotPos, ref walls, ref boxes, dir);
       }
+
+      y++;
     }
 
-    return totalExcludedPoints.ToString();
+    var total = 0;
+    foreach (var box in boxes)
+    {
+      total += box.X + (box.Y * 100);
+    }
+    
+    return total.ToString();
   }
 
   protected override string Problem2(string[] input, bool isTestInput)
   {
-    var allSensors = new List<Sensor>();
-      
-    var boundaries = isTestInput ? 20 : 4_000_000;
-
-    foreach (var line in input)
-    {
-      var checkResults = ParseLine(line);
-      allSensors.Add(checkResults.s);
-    }
-      
-    foreach (var sensor in allSensors)
-    {
-      var pointsToCheck = sensor.GetPointsAroundPerimeter()
-        .Where(p => p.X >= 0 && p.Y >= 0 && p.X <= boundaries && p.Y <= boundaries);
-
-      foreach (var checkPoint in pointsToCheck)
-      {
-        if (!allSensors.Any(s => s.IsWithinRange(checkPoint)))
-        {
-          // match found
-          var signal = ((long)checkPoint.X * 4_000_000) + checkPoint.Y; //this overflows on an int :'(
-          return signal.ToString();
-        }
-      }
-    }
-
-    return "busted";
+    throw new NotImplementedException();
   }
 
-  private static readonly Regex LineParser =
-    new(
-      "Sensor at x=([-\\d]+), y=([-\\d]+): closest beacon is at x=([-\\d]+), y=([-\\d]+)",
-      RegexOptions.Compiled);
-
-  private static (Sensor s, GridPoint b) ParseLine(string line)
+  private static void AttemptOneMove(ref GridPoint robot, ref HashSet<GridPoint> walls, ref HashSet<GridPoint> boxes,
+    GridPoint direction)
   {
-    var match = LineParser.Matches(line).First();
-    var sX = int.Parse(match.Groups[1].Value);
-    var sY = int.Parse(match.Groups[2].Value);
-    var bX = int.Parse(match.Groups[3].Value);
-    var bY = int.Parse(match.Groups[4].Value);
+    var attemptedPosition = robot + direction;
 
-    var beacon = new GridPoint(bX, bY);
-
-    return (new Sensor(new GridPoint(sX, sY), beacon), beacon);
-  }
-
-  private class Sensor
-  {
-    public readonly GridPoint Location;
-    public readonly int Distance;
-
-    public Sensor(GridPoint location, GridPoint beacon)
+    if (walls.Contains(attemptedPosition))
     {
-      Location = location;
-      Distance = CalculateDistanceToPoint(beacon);
+      //no-op
     }
-
-    public int CalculateDistanceToPoint(GridPoint other)
+    else if (boxes.Contains(attemptedPosition))
     {
-      var xDist = Math.Abs(Location.X - other.X);
-      var yDist = Math.Abs(Location.Y - other.Y);
+      //try moving boxes
+      var attemptedBoxMove = attemptedPosition + direction;
+      while (boxes.Contains(attemptedBoxMove))
+        attemptedBoxMove += direction;
 
-      return xDist + yDist;
-    }
-
-    public bool IsWithinRange(GridPoint other)
-    {
-      return CalculateDistanceToPoint(other) <= Distance;
-    }
-
-    public bool IsWithinVerticalRange(int vertical)
-    {
-      return Math.Abs(vertical - Location.Y) <= Distance;
-    }
-
-    public IEnumerable<GridPoint> GetPointsAroundPerimeter()
-    {
-      var curY = Location.Y - Distance - 1;
-      yield return new GridPoint(Location.X, Location.Y - Distance - 1);
-
-      curY++;
-      var curLeft = Location.X - 1;
-      var curRight = Location.X + 1;
-
-      //expand
-      while (curY <= Location.Y)
+      if (!walls.Contains(attemptedBoxMove))
       {
-        yield return new GridPoint(curLeft, curY);
-        yield return new GridPoint(curRight, curY);
-
-        curY++;
-        curLeft--;
-        curRight++;
+        boxes.Remove(attemptedPosition);
+        boxes.Add(attemptedBoxMove);
+        robot = attemptedPosition;
       }
-
-      //contract
-      while (curY <= Location.Y + Distance)
-      {
-        yield return new GridPoint(curLeft, curY);
-        yield return new GridPoint(curRight, curY);
-
-        curY++;
-        curLeft++;
-        curRight--;
-      }
-
-      yield return new GridPoint(Location.X, Location.Y + Distance + 1);
+    }
+    else
+    {
+      robot = attemptedPosition;
     }
   }
 }
