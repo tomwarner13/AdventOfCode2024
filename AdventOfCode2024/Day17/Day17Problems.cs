fe870583e76e0b1750fc8c7415c8598cb27d4232
Tom Warner <tomwarner13@gmail.com>
2024-12-18 09:44:18 -0500

    day 17 part 2: MAKE EVERYTHING A LONG. NO INTEGERS EVER AGAIN. REGULAR SIZE INTEGERS BANE OF EXISTENCE

--- AdventOfCode2024/Day17/Day17Problems.cs
+++ AdventOfCode2024/Day17/Day17Problems.cs
@@ -1,4 +1,4 @@
-using System.Runtime.CompilerServices;
+using System.Text;
 using AdventOfCode2024.Util;
 
 namespace AdventOfCode2024.Day17;
@@ -19,7 +19,7 @@ Program: 0,1,5,4,3,0";
     var registerB = StringUtils.ExtractIntsFromString(input[1]).First();
     var registerC = StringUtils.ExtractIntsFromString(input[2]).First();
     
-    var instructions = StringUtils.ExtractIntsFromString(input[4]).ToArray();
+    var instructions = StringUtils.ExtractLongsFromString(input[4]).ToArray();
     
     var compy = new ChronospatialComputer(registerA, registerB, registerC, instructions);
     var output = compy.Operate();
@@ -28,30 +28,101 @@ Program: 0,1,5,4,3,0";
 
   protected override string Problem2(string[] input, bool isTestInput)
   {
-    throw new NotImplementedException();
+    if (isTestInput)
+      input = new[]
+      {
+        "0",
+        "0",
+        "0",
+        "",
+        "0,3,5,4,3,0"
+      };
+    
+    var instructions = StringUtils.ExtractLongsFromString(input[4]).ToArray();
+    
+    var printer = new StringBuilder();
+    
+    var compy = new ChronospatialComputer(0, 0, 0, instructions);
+    var output = compy.FindLowestQuineInput();
+    //printer.AppendLine(string.Join(',', output.Select(n => n.ToString())));
+    printer.AppendLine(output.ToString());
+    return printer.ToString();
   }
 
   private class ChronospatialComputer
   {
-    public int RegisterA { get; set; }
-    public int RegisterB { get; set; }
-    public int RegisterC { get; set; }
+    public long RegisterA { get; set; }
+    public long RegisterB { get; set; }
+    public long RegisterC { get; set; }
 
-    private int[] _input;
-    private int _pointer;
-    private List<int> _output = new();
+    private long[] _input;
+    private long _pointer;
+    private List<long> _output = new();
+    private StringBuilder? _printer = null;
 
-    public ChronospatialComputer(int a, int b, int c, int[] input)
+    public ChronospatialComputer(long a, long b, long c, long[] input, StringBuilder? printer = null)
     {
       RegisterA = a;
       RegisterB = b;
       RegisterC = c;
       _input = input;
       _pointer = 0;
+      _printer = printer;
     }
 
-    public List<int> Operate()
+    public long FindLowestQuineInput()
     {
+      var result = FindLowestQuine(new List<long>());
+      return GetLongFromOctalList(result ?? new List<long>(), _input.Length);
+    }
+
+    private List<long>? FindLowestQuine(List<long> knownValues)
+    {
+      for (var testDigit = 0; testDigit < 8; testDigit++)
+      {
+        var testValues = new List<long>(knownValues) { testDigit };
+
+        var inputLong = GetLongFromOctalList(testValues, _input.Length);
+        var inputResult = OperateAndCleanup(inputLong);
+        if(inputResult.SequenceEqual(_input)) 
+          return testValues;
+        if(inputResult.Length != _input.Length) continue;
+        
+        //check if digits at significance are correct, if so, recurse
+        var shouldRecurse = true;
+        for (var i = 1; i <= testValues.Count; i++)
+        {
+          var lastIndexToCheck = inputResult.Length - i;
+          if (inputResult[lastIndexToCheck] != _input[lastIndexToCheck])
+          {
+            shouldRecurse = false;
+            break;
+          }
+        }
+
+        if (shouldRecurse)
+        {
+          var recursionResult = FindLowestQuine(testValues);
+          if(recursionResult != null) 
+            return recursionResult;
+        }
+      }
+
+      return null;
+    }
+
+    private void Reset()
+    {
+      RegisterA = 0;
+      RegisterB = 0;
+      RegisterC = 0;
+      _pointer = 0;
+      _output.Clear();
+    }
+
+    public List<long> Operate()
+    {
+      _printer?.AppendLine($"init: A: {RegisterA} B: {RegisterB} C: {RegisterC}");
       while (_pointer < _input.Length)
       {     
         var opcode = _input[_pointer];
@@ -63,7 +134,16 @@ Program: 0,1,5,4,3,0";
       return _output;
     }
 
-    private void Iterate(int opcode, int rawOperand)
+    private long[] OperateAndCleanup(long startingA)
+    {
+      RegisterA = startingA;
+      Operate();
+      var result = _output.ToArray();
+      Reset();
+      return result;
+    }
+
+    private void Iterate(long opcode, long rawOperand)
     {
       switch (opcode)
       {
@@ -96,60 +176,69 @@ Program: 0,1,5,4,3,0";
       }
     }
 
-    private void Adv(int rawOperand)
+    private void Adv(long rawOperand)
     {
-      RegisterA = (int)(RegisterA / Math.Pow(2, ComboOperand(rawOperand)));
+      RegisterA = (long)(RegisterA / Math.Pow(2, ComboOperand(rawOperand)));
       _pointer += 2;
+      PrintState("adv", rawOperand, ComboOperand(rawOperand));
     }
     
-    private void Bxl(int rawOperand)
+    private void Bxl(long rawOperand)
     {
       RegisterB ^= rawOperand;
       _pointer += 2;
+      PrintState("bxl", rawOperand);
     }
     
-    private void Bst(int rawOperand)
+    private void Bst(long rawOperand)
     {
       RegisterB = ComboOperand(rawOperand) % 8;
       _pointer += 2;
+      PrintState("bst", rawOperand, ComboOperand(rawOperand));
     }
     
-    private void Jnz(int rawOperand)
+    private void Jnz(long rawOperand)
     {
       if (RegisterA == 0)
       {
         _pointer += 2;
+        PrintState("jnz", rawOperand);
         return;
       }
 
       _pointer = rawOperand;
+      PrintState("jnz", rawOperand);
     }
     
-    private void Bxc(int rawOperand)
+    private void Bxc(long rawOperand)
     {
       RegisterB ^= RegisterC;
       _pointer += 2;
+      PrintState("bxc", rawOperand);
     }
     
-    private void Out(int rawOperand)
+    private void Out(long rawOperand)
     {
-      _output.Add(ComboOperand(rawOperand) % 8);
+      _output.Add((long)(ComboOperand(rawOperand) % 8));
       _pointer += 2;
+      PrintState("out", rawOperand, ComboOperand(rawOperand));
     }
     
-    private void Bdv(int rawOperand)
+    private void Bdv(long rawOperand)
     {
-      RegisterB = (int)(RegisterA / Math.Pow(2, ComboOperand(rawOperand)));
+      RegisterB = (long)(RegisterA / Math.Pow(2, ComboOperand(rawOperand)));
       _pointer += 2;
+      PrintState("bdv", rawOperand, ComboOperand(rawOperand));
     }
     
-    private void Cdv(int rawOperand)
+    private void Cdv(long rawOperand)
     {
-      RegisterC = (int)(RegisterA / Math.Pow(2, ComboOperand(rawOperand)));
+      RegisterC = (long)(RegisterA / Math.Pow(2, ComboOperand(rawOperand)));
       _pointer += 2;
+      PrintState("cdv", rawOperand, ComboOperand(rawOperand));
     }
 
-    private int ComboOperand(int rawOperand)
+    private long ComboOperand(long rawOperand)
     {
       if (rawOperand is < 0 or > 6)
         throw new ThisShouldNeverHappenException($"operand out of range: {rawOperand}");
@@ -167,6 +256,25 @@ Program: 0,1,5,4,3,0";
         return RegisterC;
       
       throw new ThisShouldNeverHappenException($"operand impossibly out of range: {rawOperand}");
+    }
+
+    private void PrintState(string op, long rawOperand, long? operand = null)
+    {
+      _printer?.AppendLine($"Ptr: {_pointer} A: {RegisterA} B: {RegisterB} C: {RegisterC} LastOp: {op}: Raw: {rawOperand} Operand: {operand ?? rawOperand}");
+    }
+
+    //i'm doing leftpad without calling a dependency here, Node devs devastated
+    private static long GetLongFromOctalList(List<long> octalValues, long numDigits)
+    {
+      var result = 0L;
+      numDigits--;
+      foreach (var octalValue in octalValues)
+      {
+        result += octalValue * (long)Math.Pow(8, numDigits);
+        numDigits--;
+      }
+
+      return result;
     }
   }
 }
